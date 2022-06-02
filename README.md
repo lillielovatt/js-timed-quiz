3d8ade01b8ef8891de8f3ff391ba35789ab1dd1a
lillielove <lillielove@gmail.com>
2022-06-01 21:12:26 -0700

    test with README file and using images

--- README.md
+++ README.md
@@ -3,6 +3,21 @@
 By: Lillie Lovatt
 5/30/2022
 
+https://github.com/lillielovatt/js-timed-quiz
+https://lillielovatt.github.io/js-timed-quiz/
+
+
+
+**********Front Page**********
+![frontpage](https://user-images.githubusercontent.com/104483166/171551507-a327287e-ab5f-4b7d-8c2a-b8ba77f6225e.png)
+
+**********Front Page**********
+
+
+
+
+
+
 AS A coding boot camp student
 I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
 SO THAT I can gauge my progress compared to my peers
--- assets/notes.js
+++ assets/notes.js
@@ -6,20 +6,17 @@ var allAnswersEl;
 var highScoreStorage = [];
 var modalCount = 0;
 
-// submitting your initials
-var formEl = document.querySelector("#submit-form");
-
-// all page elements -> front page, question page, end quiz page, last page, highscores on last page
-var frontPageEl = document.getElementById("front-page");
-var questionDisplayEl = document.getElementById("question-display");
-var endQuizEl = document.querySelector("#end-quiz");
-var lastPageEl = document.getElementById("last-page");
 var highScoreDisplayEl = document.querySelector(".high-scores");
-// all button elements
+var highScoreViewButtonEl = document.querySelector(".view-high-scores");
+var formEl = document.querySelector("#submit-form");
+var endQuizEl = document.querySelector("#end-quiz");
+
 var startOverButtonEl = document.getElementById("start-over");
 var clearHighScoreButtonEl = document.getElementById("clear-scores");
 var startQuizButtonEl = document.getElementById("start-quiz");
-var highScoreViewButtonEl = document.querySelector(".view-high-scores");
+var frontPageEl = document.getElementById("front-page");
+var lastPageEl = document.getElementById("last-page");
+var questionDisplayEl = document.getElementById("question-display");
 
 // creates span to be filled with "right" or "wrong" in response to answers
 var rightOrWrongEl = document.createElement("span");
@@ -27,6 +24,14 @@ var rightOrWrongEl = document.createElement("span");
 // adds timer to the header, currently empty--firstPage function makes it "Time: 0"
 var timer = document.createElement("p");
 document.querySelector("header").appendChild(timer);
+
+// fills in the high scores
+var highScoreListGl = JSON.parse(localStorage.getItem("highscore"));
+for (var i = 0; i < JSON.parse(localStorage.getItem("highscore")).length; i++) {
+    var highScoreEachEl = document.createElement("li");
+    highScoreEachEl.innerText = " " + highScoreListGl[i].initials + " - " + highScoreListGl[i].highScore
+    highScoreDisplayEl.appendChild(highScoreEachEl);
+}
 
 // timer, with 60 seconds
 function finishedTimer() {
@@ -103,49 +108,46 @@ var questionList = [
     },
 ]
 
-// displays questions, with argument 1 object from questionList array of objects (i.e., cycles through 1 question at a time)
+// displays questions, with argument 1 object from questionList array of objects
 var displayQuestion = function (questionObj) {
-    // allows for questions to be displayed, turning off class="hidden"
     questionDisplayEl.className = "question-display";
-    // selecting h1 element for question, and ul element for answers
     var questionAskedEl = document.querySelector(".question-asked");
     var possibleAnswersEl = document.querySelector(".possible-answers");
+
     // while loop that clears out previous answers
     while (possibleAnswersEl.firstChild) {
         possibleAnswersEl.removeChild(possibleAnswersEl.lastChild);
     }
+
     // create elements that hold the question, and then each answer. 
     questionAskedEl.innerText = questionObj.question;
     var answerKeys = Object.keys(questionObj.answers) // ['a','b','c','d']
+
     // loops through and creates answer elements in HTML 
     for (var i = 0; i < answerKeys.length; i++) {
         const answerKey = answerKeys[i]; //A, B, C, D
         const answerItem = questionObj.answers[answerKey]; //WORDS
-        // creates li element, filled with the possible answer
         var answerEl = document.createElement("li");
         answerEl.innerText = answerItem;
         answerEl.className = "answer-choice";
-        // assigns data attribute to allow for correct answer matching later
+
         answerEl.setAttribute("data-question-id", questionCount);
         answerEl.setAttribute("data-answer-id", answerKey);
-        // makes sure the li elements appear on the page by appending to ol
+
         possibleAnswersEl.appendChild(answerEl);
     }
-    // adds event listener to each answer, and checks if they are correct in the answerSelectionHandler function
+    // adds event listener to each answer, and checks if they are correct in the called function
     var allAnswersEl = document.querySelector(".possible-answers");
     allAnswersEl.addEventListener("click", answerSelectionHandler);
 };
 
 // checks to see which answer is correct
 var answerSelectionHandler = function (event) {
-    // shows us EXACTLY where we clicked, retrieves entire HTML for click event
     var targetEl = event.target;
-    // gets data attribute, automatically parses name in Camel style
     var questionIndex = targetEl.dataset.questionId; //should be the same as questionCount, number of question ID
     var answerIndex = targetEl.dataset.answerId; //should be correct answer letter.
-    // if answer is "right" or "wrong" starts as BLANK
     rightOrWrongEl.innerText = "";
-    // if data attribute value for user's answer MATCHES correct answer, display right. Else, wrong and deduct time.
+
     if (questionList[questionIndex].correctAnswer === answerIndex) {
         rightOrWrongEl.innerText = "Right!";
         questionDisplayEl.appendChild(rightOrWrongEl);
@@ -154,9 +156,7 @@ var answerSelectionHandler = function (event) {
         questionDisplayEl.appendChild(rightOrWrongEl);
         countSec -= 5;
     }
-    // ensures we move to next question, shows this question has ended
     questionCount++;
-    // if we've reached the end of the question list, or time is out, then we end the quiz.
     if (questionCount >= questionList.length || countSec <= 0) {
         endQuiz();
     }
@@ -165,64 +165,47 @@ var answerSelectionHandler = function (event) {
     }
 };
 
-// called when "start quiz" button is clicked
 var startQuiz = function () {
     // front-page should be hidden 
     frontPageEl.className = "hidden";
-    // initializes time available, and starts timer, and calls first question.
     countSec = 60;
     setTimeout(finishedTimer, howLongToWait);
     displayQuestion(questionList[questionCount]);
 };
 
-// called when timer runs out, or when questions run out
 var endQuiz = function () {
-    // removes "right" or "wrong", else it'll appear next round
     rightOrWrongEl.innerText = "";
     timer.innerText = "Time's up!";
-    // hides the questions element
     document.getElementById("question-display").className = "hidden";
-    // makes sure end quiz page is displayed, but changing from class="hidden"
     endQuizEl.className = "end-quiz";
     var finalScoreEl = document.querySelector(".final-score");
     finalScoreEl.innerText = "Your final score is " + countSec + ".";
 };
 
-// called when "clear high scores" button is clicked
 var clearStorage = function () {
     localStorage.clear();
-    // if you choose to clear the storage, then automatically remove all high scores from HTML
     while (highScoreDisplayEl.firstChild) {
         highScoreDisplayEl.removeChild(highScoreDisplayEl.lastChild);
     }
 };
 
-// called for last page, after form submit button clicked 
 var highScorePage = function () {
-    // hiding end quiz page, having last page appear
     endQuizEl.className = "hidden";
     lastPageEl.className = "last-page";
-    // clear highScoreDisplay HTML or else the for loop will write it all over again, and again
-    while (highScoreDisplayEl.firstChild) {
-        highScoreDisplayEl.removeChild(highScoreDisplayEl.lastChild);
-    }
-    // makes the high score array of objects NOT a string, when getting from local storage
+    
     var highScoreList = JSON.parse(localStorage.getItem("highscore"));
-    for (var i = 0; i < highScoreList.length; i++) {
-        // creates list element, fills with initials and high scores, and adds to the page
-        var highScoreEachEl = document.createElement("li");
-        highScoreEachEl.innerText = " " + highScoreList[i].initials + " - " + highScoreList[i].highScore
-        highScoreDisplayEl.appendChild(highScoreEachEl);
-    }
+    console.log(highScoreList);
+    var latestScore = highScoreList.length - 1;
+    console.log(highScoreList[latestScore]);
+    var highScoreEachEl = document.createElement("li");
+    highScoreEachEl.innerText = " " + highScoreList[latestScore].initials + " - " + highScoreList[latestScore].highScore
+    highScoreDisplayEl.appendChild(highScoreEachEl);
 }
 
-// called after form submit button clicked to capture input for localStorage
 var setStorage = function (event) {
-    event.preventDefault(); //stop it from refreshing the page
-    // takes user input of initials
+    event.preventDefault();
     var initialsInput = document.querySelector("input[name='submit-initials']").value;
     document.querySelector("input[name='submit-initials']").value = "";
-    // validation
     if (!initialsInput || initialsInput.length > 4 || !isNaN(initialsInput)) {
         alert("You need to fill out your intials! Please enter 4 letters or less.");
         return;
@@ -232,18 +215,33 @@ var setStorage = function (event) {
         initials: initialsInput,
         highScore: countSec
     };
-    // highScoreStorage takes all items in local storage (or if empty, creates an empty array), adds newest score and initials, sorts it
     highScoreStorage = JSON.parse(localStorage.getItem("highscore")) || [];
     highScoreStorage.push(highScoreDataObj);
     highScoreStorage.sort(function (a, b) {
         return b.highScore - a.highScore;
     });
-    // stores sorted initials/high scores objects in an highscorestorage array, and sends to localstorage
     localStorage.setItem("highscore", JSON.stringify(highScoreStorage));
     highScorePage();
 };
 
-// called when page first loads, and called again when "start over" button is clicked
+// [
+//     {initials: "LL", 
+//     highScore:50
+//     },
+//     {initials: "KK", 
+//     highScore:150
+//     },
+// ];
+
+// this is why you shouldn't do an array, corruption of data harder to find--where Mac's 9000 score is given to AA
+// [
+//     {initials: ["LL","KK","MM", "AA", "Mac"], 
+//     highScore:[50,100,150,9000]
+//     }
+// ];
+
+
+
 var frontPage = function () {
     // display front page
     frontPageEl.className = "front-page";
@@ -257,12 +255,9 @@ var frontPage = function () {
     // front page remains on screen until user clicks startQuizButtonEl
 };
 
-// creates modal that pops out at any time for viewing high scores
 var viewHighScorePage = function () {
-    // creates modal on the page, makes sure it's displayed
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
-    // similiar to highScorePage function, just appends high scores to the modal ol
     var highScoreList = document.querySelector(".modal-content-p");
     var highScoreListStorage = JSON.parse(localStorage.getItem("highscore"));
     for (var i = 0; i < JSON.parse(localStorage.getItem("highscore")).length; i++) {
@@ -270,11 +265,9 @@ var viewHighScorePage = function () {
         highScoreEachEl.innerText = " " + highScoreListStorage[i].initials + " - " + highScoreListStorage[i].highScore
         highScoreList.appendChild(highScoreEachEl);
     }
-    // when you click out of the modal, it closes
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
-            // clears out the display so that way when you call it again, it doesn't append over and over again
             while (highScoreList.firstChild) {
                 highScoreList.removeChild(highScoreList.lastChild);
             }
@@ -294,3 +287,61 @@ clearHighScoreButtonEl.addEventListener("click", clearStorage);
 highScoreViewButtonEl.addEventListener("click", viewHighScorePage);
 
 frontPage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var questions = [
+//     {
+//         "question": "What is the maori translation for: \"Apple\"?",
+//         "PossibleAnswers":
+//             [{ "text": "Aporo", "correct": true },
+//             { "text": "Ako", "correct": false },
+//             { "text": "Whanaunga", "correct": false },
+//             { "text": "Pene rākau", "correct": false }
+//             ]
+//     },
+//     {
+//         "question": "How many fingers on a hand?",
+//         "PossibleAnswers":
+//             [{ "text": "3", "correct": true },
+//             { "text": "5", "correct": false },
+//             { "text": "10", "correct": false },
+//             { "text": "4 and 1 thumb", "correct": true }
+//             ]
+//     },
+//     {
+//         "question": "What is the maori translation for the french word: \"Pomme\"",
+//         "PossibleAnswers":
+//             [{ "text": "Ako", "correct": false },
+//             { "text": "Whanaunga", "correct": false },
+//             { "text": "Aporo", "correct": true },
+//             { "text": "Pene rākau", "correct": false }
+//             ]
+//     }
+// ]
+// based code on structure of above^^
+
+
+// var header = $("<header>").addClass("header");
+// var timer = $("<p>")
+//       .text(countSec);
+// header.append(timer);
+// $(this).append(header);
+// why didn't this work???
+
+
+
+
