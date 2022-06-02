616e6897b96b0713dadd0f893e91d3fefac42433
lillielove <lillielove@gmail.com>
2022-06-01 17:32:58 -0700

    final working product, usable, local storage problem fixed, CSS style sheet finished

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,37 @@
+// initializing global variables
 var countSec;
 var questionCount;
 var howLongToWait = 1000;
+var allAnswersEl;
 var highScoreStorage = [];
+var modalCount = 0;
 
-
+var highScoreDisplayEl = document.querySelector(".high-scores");
+var highScoreViewButtonEl = document.querySelector(".view-high-scores");
 var formEl = document.querySelector("#submit-form");
+var endQuizEl = document.querySelector("#end-quiz");
 
 var startOverButtonEl = document.getElementById("start-over");
 var clearHighScoreButtonEl = document.getElementById("clear-scores");
-
 var startQuizButtonEl = document.getElementById("start-quiz");
 var frontPageEl = document.getElementById("front-page");
-
 var lastPageEl = document.getElementById("last-page");
-
-var allAnswersEl;
 var questionDisplayEl = document.getElementById("question-display");
+
+// creates span to be filled with "right" or "wrong" in response to answers
 var rightOrWrongEl = document.createElement("span");
- 
-var endQuizEl = document.querySelector("#end-quiz");
 
-
-
-
-// var header = $("<header>").addClass("header");
-// var timer = $("<p>")
-//       .text(countSec);
-// header.append(timer);
-// $(this).append(header);
-// why didn't this work???
-
+// adds timer to the header, currently empty--firstPage function makes it "Time: 0"
 var timer = document.createElement("p");
 document.querySelector("header").appendChild(timer);
+
+// fills in the high scores
+var highScoreListGl = JSON.parse(localStorage.getItem("highscore"));
+for (var i = 0; i < JSON.parse(localStorage.getItem("highscore")).length; i++) {
+    var highScoreEachEl = document.createElement("li");
+    highScoreEachEl.innerText = " " + highScoreListGl[i].initials + " - " + highScoreListGl[i].highScore
+    highScoreDisplayEl.appendChild(highScoreEachEl);
+}
 
 // timer, with 60 seconds
 function finishedTimer() {
@@ -110,7 +110,7 @@ var questionList = [
 
 // displays questions, with argument 1 object from questionList array of objects
 var displayQuestion = function (questionObj) {
-    questionDisplayEl.className="question-display";
+    questionDisplayEl.className = "question-display";
     var questionAskedEl = document.querySelector(".question-asked");
     var possibleAnswersEl = document.querySelector(".possible-answers");
 
@@ -119,10 +119,10 @@ var displayQuestion = function (questionObj) {
         possibleAnswersEl.removeChild(possibleAnswersEl.lastChild);
     }
 
-    // create elements that hold question, and then each answer. 
+    // create elements that hold the question, and then each answer. 
     questionAskedEl.innerText = questionObj.question;
     var answerKeys = Object.keys(questionObj.answers) // ['a','b','c','d']
-  
+
     // loops through and creates answer elements in HTML 
     for (var i = 0; i < answerKeys.length; i++) {
         const answerKey = answerKeys[i]; //A, B, C, D
@@ -136,11 +136,12 @@ var displayQuestion = function (questionObj) {
 
         possibleAnswersEl.appendChild(answerEl);
     }
+    // adds event listener to each answer, and checks if they are correct in the called function
     var allAnswersEl = document.querySelector(".possible-answers");
     allAnswersEl.addEventListener("click", answerSelectionHandler);
 };
 
-
+// checks to see which answer is correct
 var answerSelectionHandler = function (event) {
     var targetEl = event.target;
     var questionIndex = targetEl.dataset.questionId; //should be the same as questionCount, number of question ID
@@ -157,7 +158,6 @@ var answerSelectionHandler = function (event) {
     }
     questionCount++;
     if (questionCount >= questionList.length || countSec <= 0) {
-        rightOrWrongEl.innerText = "";
         endQuiz();
     }
     else {
@@ -166,61 +166,49 @@ var answerSelectionHandler = function (event) {
 };
 
 var startQuiz = function () {
-    // class=front-page should be hidden - by adding class hidden
+    // front-page should be hidden 
     frontPageEl.className = "hidden";
-    countSec=60;
+    countSec = 60;
     setTimeout(finishedTimer, howLongToWait);
     displayQuestion(questionList[questionCount]);
 };
 
 var endQuiz = function () {
+    rightOrWrongEl.innerText = "";
     timer.innerText = "Time's up!";
-    document.getElementById("question-display").className="hidden";
-    endQuizEl.className="end-quiz";
+    document.getElementById("question-display").className = "hidden";
+    endQuizEl.className = "end-quiz";
     var finalScoreEl = document.querySelector(".final-score");
     finalScoreEl.innerText = "Your final score is " + countSec + ".";
 };
 
 var clearStorage = function () {
     localStorage.clear();
+    while (highScoreDisplayEl.firstChild) {
+        highScoreDisplayEl.removeChild(highScoreDisplayEl.lastChild);
+    }
 };
 
-var highScorePage=function(){
-    endQuizEl.className="hidden";
-    lastPageEl.className="last-page";
-    // wanted to sort them but will discuss later, could not work out
-    // for(var i=0; i<highScore.length;i++){
-    //     if(highScore.length===1){
-    //         // display just the 1 score as high score, done
-    //     }
-    //     else{
-    //         if(highScore[highScore.length-1]>highScore[i]){
-
-    //         }
-    //     }
-    // }
-    for(var i=0; i<highScoreStorage.length;i++){
-        var highScoreDisplayEl = document.querySelector(".high-scores");
-        var highScoreEachEl = document.createElement("li");
-        highScoreEachEl.innerText= " " + highScoreStorage[i].initials + " - " + highScoreStorage[i].highScore
-        highScoreDisplayEl.appendChild(highScoreEachEl);
-    }
-
-
+var highScorePage = function () {
+    endQuizEl.className = "hidden";
+    lastPageEl.className = "last-page";
+    
+    var highScoreList = JSON.parse(localStorage.getItem("highscore"));
+    var latestScore = highScoreList.length - 1;
+    var highScoreEachEl = document.createElement("li");
+    highScoreEachEl.innerText = " " + highScoreList[latestScore].initials + " - " + highScoreList[latestScore].highScore
+    highScoreDisplayEl.appendChild(highScoreEachEl);
 }
 
 var setStorage = function (event) {
     event.preventDefault();
     var initialsInput = document.querySelector("input[name='submit-initials']").value;
-    if (!initialsInput) {
-        alert("You need to fill out your intials!");
+    document.querySelector("input[name='submit-initials']").value = "";
+    if (!initialsInput || initialsInput.length > 4 || !isNaN(initialsInput)) {
+        alert("You need to fill out your intials! Please enter 4 letters or less.");
         return;
     }
-    if (initialsInput.length > 4) {
-        alert("You need to fill out your intials, not your name!");
-        return;
-    }
-    initialsInput=initialsInput.toUpperCase();
+    initialsInput = initialsInput.toUpperCase();
     var highScoreDataObj = {
         initials: initialsInput,
         highScore: countSec
@@ -235,14 +223,34 @@ var frontPage = function () {
     // display front page
     frontPageEl.className = "front-page";
     // hide end quiz page and last page
-    lastPageEl.className="hidden";
-    endQuizEl.className="hidden";
+    lastPageEl.className = "hidden";
+    endQuizEl.className = "hidden";
     // timer not yet on, question count not yet started
     timer.innerText = "Time: 0";
     questionCount = 0;
 
     // front page remains on screen until user clicks startQuizButtonEl
 };
+
+var viewHighScorePage = function () {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    var highScoreList = document.querySelector(".modal-content-p");
+    var highScoreListStorage = JSON.parse(localStorage.getItem("highscore"));
+    for (var i = 0; i < JSON.parse(localStorage.getItem("highscore")).length; i++) {
+        var highScoreEachEl = document.createElement("li");
+        highScoreEachEl.innerText = " " + highScoreListStorage[i].initials + " - " + highScoreListStorage[i].highScore
+        highScoreList.appendChild(highScoreEachEl);
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            while (highScoreList.firstChild) {
+                highScoreList.removeChild(highScoreList.lastChild);
+            }
+        }
+    }
+}
 
 // start quiz  button
 startQuizButtonEl.addEventListener("click", startQuiz);
@@ -252,6 +260,8 @@ formEl.addEventListener("submit", setStorage);
 startOverButtonEl.addEventListener("click", frontPage);
 // clear high score button
 clearHighScoreButtonEl.addEventListener("click", clearStorage);
+// "view high scores" selection on upper left of screen
+highScoreViewButtonEl.addEventListener("click", viewHighScorePage);
 
 frontPage();
 
@@ -302,6 +312,12 @@ frontPage();
 // based code on structure of above^^
 
 
+// var header = $("<header>").addClass("header");
+// var timer = $("<p>")
+//       .text(countSec);
+// header.append(timer);
+// $(this).append(header);
+// why didn't this work???
 
 
 
